ecad6c9fb4de4e70249dc6ccdbd0db44f30e5dd2
shalinthomas530 <156893062+shalinthomas530@users.noreply.github.com>
2024-11-14 22:49:17 +0200

    changed all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react"
+import Header from "../components/header"
+import Meme from "../components/Meme"
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div>
+      <Header/>
+      <Meme/>
+    </div>
   )
 }
 
 export default App
+
